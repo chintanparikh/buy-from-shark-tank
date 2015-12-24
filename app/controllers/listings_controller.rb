5075309fcbffb0455281c5d3d38f89f1385170a0
Chintan Parikh <chintan@pennywhale.com>
2015-12-24 17:02:56 -0500

    Functiona:

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,25 @@
 class ListingsController < ApplicationController
   def index
-    @listings = Listing.all.order("CREATED_AT DESC").page(params[:page])
+    page = params[:page]
+    search_term = params[:search_term]
+    season = params[:season]
+    episode = params[:episode]
+
+    @listings = Listing.all
+  
+    if (season and season != 'Season')
+      @listings = @listings.where(season: season)
+    end
+
+    if (episode and episode != 'Episode')
+      @listings = @listings.where(episode: episode)
+    end
+
+    if (search_term and search_term.present? and !search_term.blank?)
+      @listings = @listings.where("name ILIKE ?", "%#{search_term}%")
+    end
+
+
+    @listings = @listings.order("CREATED_AT DESC").page(params[:page])
   end
 end
